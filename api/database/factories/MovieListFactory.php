bb23206bd792faabaf01a4a9969920bdbad3b965
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-19 00:38:32 +0200

    MovieListFactory

---
+++ api/database/factories/MovieListFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class MovieListFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->words(3, true),
+            'description' => fake()->sentence(),
+            'user_id' => User::factory(),
+        ];
+    }
+}

2af6ec042ae109e76e5e09cee1bfbfbfa4cda4df
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-19 20:01:26 +0200

    Rating ShowResouce

---
+++ api/app/Http/Resources/Movie/Rating/ShowResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Movie\Rating;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShowResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'rating' => $this->rating,
+            'movie_id' => $this->movie_id,
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+                'username' => $this->user->username,
+            ],
+            'created_at' => $this->created_at?->format('Y-m-d H:i'),
+        ];
+    }
+}

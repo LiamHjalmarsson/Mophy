d6342f08d44fc988d20aaf3140826766bf458679
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-21 10:56:03 +0200

    Miss some category change to Genre

--- api/app/Policies/CategoryPolicy.php
+++ api/app/Policies/CategoryPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Category;
+use App\Models\Genre;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class CategoryPolicy
+class GenrePolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class CategoryPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Category $category): bool
+    public function view(User $user, Genre $genre): bool
     {
         return true;
     }
@@ -35,7 +35,7 @@ class CategoryPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Category $category): bool
+    public function update(User $user, Genre $genre): bool
     {
         return $user->is_admin;
     }
@@ -43,7 +43,7 @@ class CategoryPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Category $category): bool
+    public function delete(User $user, Genre $genre): bool
     {
         return $user->is_admin;
     }
@@ -51,7 +51,7 @@ class CategoryPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Category $category): bool
+    public function restore(User $user, Genre $genre): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class CategoryPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Category $category): bool
+    public function forceDelete(User $user, Genre $genre): bool
     {
         return false;
     }
